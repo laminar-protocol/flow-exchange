401e77683705a978dbfd579af9ffbcd69c1a64d1
nantian <nantian@laminar.one>
2020-01-16 16:59:07 +0800

    add storybook

--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -3,6 +3,24 @@ import { Button } from 'antd';
 
 import * as theme from '../../theme';
 
+console.log(`
+&.ant-btn,
+&.ant-btn:hover,
+&.ant-btn:focus,
+&.ant-btn:active {
+  background: linear-gradient(90deg, ${theme.keyColorBlue} 0%, ${theme.keyColorRed} 100%);
+  color: #fff;
+  border: 0;
+  font-weight: ${theme.boldWeight};
+}
+
+&.ant-btn[disabled],
+&.ant-btn[disabled]:hover {
+  opacity: 0.75;
+  color: #fff;
+}
+`);
+
 const Component = styled(Button)`
   &.ant-btn,
   &.ant-btn:hover,
---
+++ src/components/buttons/__stories__/buttons.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Button, LightButton, PrimaryButton, SolidButton } from '..';
+
+export default {
+  title: 'Home',
+  decorators: [(storyFn: any) => <div style={{ padding: 160 }}>{storyFn()}</div>],
+};
+
+export const _Button = () => <Button>Button</Button>;
+export const _LightButton = () => <LightButton>LightButton</LightButton>;
+export const _PrimaryButton = () => <PrimaryButton>PrimaryButton</PrimaryButton>;
+export const _SolidButton = () => <SolidButton>SolidButton</SolidButton>;
