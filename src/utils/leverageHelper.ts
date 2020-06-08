e4120d3ff220301012a81a20304b88eda529c710
nantian <nantian@laminar.one>
2020-06-08 14:26:52 +1200

    pl

--- src/utils/leverageHelper.ts
+++ src/utils/leverageHelper.ts
@@ -7,7 +7,7 @@ const Map = {
   Fifty: '50',
 } as Record<string, string>;
 
-export const getLeverage = (str: string) => {
+export const getLeverage = (str: string): ['long' | 'short', string] | [] => {
   const [, direction, leverage] = str.match('^(Long|Short)(.*)$') || [];
   if (direction && Map[leverage]) {
     return [direction === 'Long' ? 'long' : 'short', Map[leverage]];
